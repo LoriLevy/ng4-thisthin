deba09a533135712ef6cd4ede32c866a9dd393b7
Lori Levy <lori@lorinet.com>
2017-10-05 21:02:26 -0700

    Added header and footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // styleUrls: ['./app.component.css']
+  template: `
+  <app-header></app-header>
+  
+  <app-footer></app-footer>
+  `
 })
 export class AppComponent {
-  title = 'app';
+  title: string = 'ThisThin';
 }
